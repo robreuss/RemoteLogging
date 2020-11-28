5c31a13262110c4c01bb69a9a1eb5a079adf8065
Rob Reuss <robreuss@mac.com>
2020-11-27 21:46:14 -0500

    Fixed position of dependency in Package

--- Package.swift
+++ Package.swift
@@ -21,9 +21,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "RemoteLogging",
-            dependencies: []),
+            dependencies: ["ElementalController"]),
         .testTarget(
             name: "RemoteLoggingTests",
-            dependencies: ["RemoteLogging", "ElementalController"]),
+            dependencies: ["RemoteLogging"]),
     ]
 )
